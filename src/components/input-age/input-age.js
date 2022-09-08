e3c9fc4b5f29395ef72dc2920b5f0fff046836dc
Pribytov <igor-pribytov@yandex.ru>
2022-09-08 20:50:59 +0300

    Добавлен компоненты выбора возраста

---
+++ src/components/input-age/input-age.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useState } from 'react';
+import './input-age.css';
+
+const InputWrapper = styled('input')`
+    color: ${props => props.isValid ? 'green' : 'red'};
+`
+
+export const InputAge = () => {
+    let refAge = React.createRef();
+    const [isValid, setIsValid] = useState(false);
+
+    const changeHandler = (e) => {
+        const MIN_NUMBER = 0;
+        const MAX_NUMBER = 150;
+
+        let val = refAge.current.value;
+        
+        if (val >= MIN_NUMBER && val <= MAX_NUMBER)
+            setIsValid(true);
+        else
+            setIsValid(false);
+    }
+
+    return (
+        <div className='input-Age'>
+            <InputWrapper
+                type='number'
+                placeholder='Введите возраст'
+                required
+                onChange={changeHandler}
+                ref={refAge}
+                min='0'
+                max='150'
+                isValid={isValid}
+            />
+        </div>
+    );
+};
