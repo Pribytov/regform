81e8fe6c135a91054e8220f64227468d658e970b
Pribytov <igor-pribytov@yandex.ru>
2022-09-08 20:52:18 +0300

    Добавлен компоненты выбора email

---
+++ src/components/input-email/input-email.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useState } from 'react';
+import './input-email.css';
+
+const InputWrapper = styled('input')`
+    color: ${props => props.isValid ? 'green' : 'red'};
+`
+
+export const InputEmail = () => {
+    let refEmail = React.createRef();
+    const [isValid, setIsValid] = useState(false);
+
+    const changeHandler = (e) => {
+        const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+        
+        let val = refEmail.current.value;
+
+        if (EMAIL_REGEXP.test(val))
+            setIsValid(true);
+        else
+            setIsValid(false);
+        console.log(isValid);
+    }
+
+    return (
+        <div className='input-Email'>
+            <InputWrapper
+                type='email'
+                placeholder='Введите email'
+                required
+                onChange={changeHandler}
+                ref={refEmail}
+                isValid={isValid}
+            />
+        </div>
+    );
+};
