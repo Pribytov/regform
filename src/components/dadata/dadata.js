7228dc599f272e578fae4c5a751e3f700829a8f5
Pribytov <igor-pribytov@yandex.ru>
2022-09-10 13:14:40 +0300

    исправлен компоненты вывода адреса

--- src/components/dadata/dadata.js
+++ src/components/dadata/dadata.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { useState } from 'react';
 import { AddressSuggestions } from 'react-dadata';
 import 'react-dadata/dist/react-dadata.css';
 
 import './dadata.css';
 
 
-export const Dadata = () => {
-    const [value, setValue] = useState();
+export const Dadata = ({updateData}) => {
+    const setValue = (e) => {
+        updateData(e.value);
+    }
 
     return (
         <div className='input-Date'>
             <label>Введите город проживания и улицу:</label>
-            <AddressSuggestions token="a19e1a7459844b0a8c491107853297016f483a4e" value={value} onChange={setValue} />
+            <AddressSuggestions
+                token="a19e1a7459844b0a8c491107853297016f483a4e"
+                onChange={setValue}
+            />
         </div>
     );
 };
