417d16638166333c8e41da7494619d1649f45ecb
Pribytov <igor-pribytov@yandex.ru>
2022-09-10 13:44:41 +0300

    добавлена страница вывода данных

---
+++ src/pages/output-page/output-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { OutForm } from '../../components/ouputform';
+import './output-page.css';
+
+export const OutputPage = () => {
+    return (
+        <div className="home-page">
+            <OutForm />
+        </div>
+    );
+};
