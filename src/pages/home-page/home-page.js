c37fdcf7cff3ea2acab4102bca26522e1ba7ad5a
Pribytov <igor-pribytov@yandex.ru>
2022-09-10 13:44:04 +0300

    добавлена домашняя страница

---
+++ src/pages/home-page/home-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { RegForm } from '../../components/regform';
+import './home-page.css';
+
+export const HomePage = () => {
+    return (
+        <div className="home-page">
+            <RegForm />
+        </div>
+    );
+};
